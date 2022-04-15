3a7a2733f2133ad35d656189a07c1d882d797824
Moe Ko <moethantkoko@gmail.com>
2022-04-15 16:51:55 -0400

    grids looking goood

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
+// Initialize the game board on page load
 
+initBoard()
+initCatRow()
+function initCatRow(){
+    let catRow = document.getElementById('category-row')
+
+    for(let j = 0; j <6; j++){
+        let box = document.createElement('div')
+        box.className = 'clue-box category-box'
+        
+        catRow.appendChild(box)
+    }
+}
+
+
+
+
+
+
+
+function initBoard(){
+    let board = document.getElementById('clue-board')
+
+    // GENERATE 5 ROWS, THEN PLACE 6 BOXES IN EACH ROW
+
+    for(let i = 0; i <5; i++){
+        let row = document.createElement('div')
+        let boxValue = 200 * (i+1)
+        row.className = 'clue-row'
+
+        for(let j = 0; j <6; j++){
+            let box = document.createElement('div')
+            box.className = 'clue-box'
+            box.textContent = '$' + boxValue
+            //box.appendChild( document.createTextNode(boxValue)) // backwards compatible
+            box.addEventListener('click',getClue,false)
+            row.appendChild(box)
+        }
+
+        board.appendChild(row)
+    }
+
+
+}
+
+function getClue() {
+    console.log("YO")
+}
