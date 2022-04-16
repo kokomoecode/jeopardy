5b88f2bab96f3383948d96d18128a05bd7a9f5bd
Moe Ko <moethantkoko@gmail.com>
2022-04-16 13:17:47 -0400

    added score checking and reset checking function

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ function initBoard(){
     }
 }
 
-
+// Call API
 
 function randInt(){
     return Math.floor(Math.random()* (18418) + 1)
@@ -53,6 +53,12 @@ let catArray = []
 
 
 function buildCategories(){
+    
+    if(!(document.getElementById('category-row').firstChild.innerText == '')){
+        resetBoard()
+    }
+
+
 
     const fetchReq1 = fetch(`https://jservice.io/api/category?&id=${randInt()}`)
     .then((res) => res.json());
@@ -83,6 +89,25 @@ function buildCategories(){
 
 }
 
+
+// RESET BOARD AND $$ AMOUNT IF NEEDED
+
+function resetBoard(){
+    let clueParent = document.getElementById('clue-board')
+    while (clueParent.firstChild){
+        clueParent.removeChild(clueParent.firstChild)
+    }
+    let catParent = document.getElementById('category-row')
+    while (catParent.firstChild){
+        catParent.removeChild(catParent.firstChild)
+    }
+    document.getElementById('score').innerText = 0
+    initBoard()
+    initCatRow()
+}
+
+
+
 //LOAD CATEGORIES TO THE BOARD
 
 function setCategories(catArray){
@@ -108,5 +133,42 @@ function getClue(event) {
     let clue = cluesList.find(obj => {
         return obj.value == boxValue
     })
+    console.log(clue)
     showQuestion(clue,child,boxValue)
 }
+
+// SHOW QUESTION TO USER AND GET THEIR ANSWER
+
+function showQuestion(clue,target,boxValue){
+    let userAnswer = prompt(clue.question).toLowerCase()
+    let correctAnswer = clue.answer.toLowerCase().replace(/<\/?[^>]+(>|$)/g, "")
+    let possiblePoints = +(boxValue)
+    target.innerHTML = clue.answer
+    target.removeEventListener('click',getClue,false)
+    evaluateAnswer(userAnswer,correctAnswer,possiblePoints)
+    
+}
+
+
+function evaluateAnswer(userAnswer,correctAnswer,possiblePoints){
+    let checkAnswer = (userAnswer == correctAnswer) ? 'correct' : 'incorrect'
+    let confirmAnswer = 
+    confirm(`For ${possiblePoints}, you answered "${userAnswer}", and correct answer was "${correctAnswer}". 
+    Your answer appears to be ${checkAnswer}. Click Ok to accept or Click Cancel if the answer was not properly evaluated.`)
+    awardPoints(checkAnswer,confirmAnswer,possiblePoints)
+}
+
+
+// AWARD POINT
+
+function awardPoints(checkAnswer,confirmAnswer, possiblePoints) {
+    if(!(checkAnswer == 'incorrect' && confirmAnswer == true)){
+        //award points
+        let target = document.getElementById('score')
+        let currentScore = +(target.innerText)
+        currentScore += possiblePoints
+        target.innerText = currentScore
+    } else {
+        alert(`No points awarded.`)
+    }
+}   
